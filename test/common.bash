4dd6a0971d47d1f193984658171f7f57a0d168d6
Jerome Charaoui <jerome@riseup.net>
2018-07-15 00:48:39 -0400

    tests: Fix setconfig() and make more robust

--- test/common.bash
+++ test/common.bash
@@ -68,9 +68,9 @@ teardown() {
 }
 
 setconfig() {
-    if grep -q "$2 =" "${BATS_TMPDIR}/$1"; then
-        sed -i "s/^$2.*/$2 = $3/" "${BATS_TMPDIR}/$1"
+    if grep -qe "$2\s\+=" "${BATS_TMPDIR}/$1"; then
+        sed -i "s/^$2\s\+=.*/$2 = $3/" "${BATS_TMPDIR}/$1"
     else
-        echo "$2 = $3/" >> "${BATS_TMPDIR}/$1"
+        echo "$2 = $3" >> "${BATS_TMPDIR}/$1"
     fi
 }
